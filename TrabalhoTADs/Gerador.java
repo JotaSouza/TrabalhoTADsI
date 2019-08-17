945f5455aaca615027785dce8f4e5322f649dcf2
Jônatas Souza <53580034+JotaSouza@users.noreply.github.com>
2019-08-16 22:30:05 -0300

    Update Gerador.java

--- TrabalhoTADs/Gerador.java
+++ TrabalhoTADs/Gerador.java
@@ -7,15 +7,25 @@ import java.util.List;
 public class Gerador {
 
 	public void aleatorioQualquer() {
+		//declaração da variável que limita a quantidade de número
 		int contador = 0;
+		//Array dos números selecionados para o teste
 		String[] numeros = {"00", "01", "02", "03", "04", "05", "06",
 				"07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"};
+		//Criação da lista de String contendo os números. A classe Array permite ao Java ver o Array como uma lista
 		List<String> list = Arrays.asList(numeros);
+		//Utilizo o método Shuffle da classe Collections, que faz com que os elementos do Array fiquem desordenados
 		Collections.shuffle(list);
+		//A partir de agora todos os elementos ficam desordenados. Mas será impresso apenas o primeiro
 		
+		//Ou seja, enquanto o contador for menor do que 1
 		while(contador < 1) {
+			//O método get seleciona qualquer elemento existente na lista
 			System.out.println(list.get(contador));
+			//Sempre que for impresso um número, o contatos é incrementado em 1 para sair do loop.
 			contador++;
 		}
 	}
+	
+	
 }
